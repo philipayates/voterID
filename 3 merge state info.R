8bee6f9bf73c678b27204b14ee4d0faa4e37a007
Phil Yates <66975068+philipayates@users.noreply.github.com>
2020-09-06 12:41:32 -0500

    Update 3 merge state info.R

--- 3 merge state info.R
+++ 3 merge state info.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 
 # Read in county data
-congressional.2018.county <- read.csv("/Volumes/PAY/Political Science/Liz 2020/Data/congressional-2018-county.csv")
+congressional.2018.county <- read.csv("congressional-2018-county.csv")
 
 # Read in file containing state information
-state.info <- read.csv("/Volumes/PAY/Political Science/Liz 2020/Data/state_info.csv")
+state.info <- read.csv("state_info.csv")
 
 # Pull the unique state FIPS for each county FIPS code
 congress.2018.state.v1 <- congressional.2018.county %>%
@@ -16,9 +16,9 @@ congress.2018.state.v2 <- congress.2018.state.v1 %>%
   left_join(state.info,id="state_fips")
 
 # Merge this with county demographics for final data set
-county.demographics <- read.csv("/Volumes/PAY/Political Science/Liz 2020/Data/county-demographics.csv")
+county.demographics <- read.csv("county-demographics.csv")
 final.data <- county.demographics %>%
   inner_join(congress.2018.state.v2,id="fips")
 final.data.v2 <- final.data %>%
   filter(!is.na(median_hh_inc))
-write_csv(final.data.v2,"/Volumes/PAY/Political Science/Liz 2020/Data/final-data-VID.csv")
+write_csv(final.data.v2,"final-data-VID.csv")
